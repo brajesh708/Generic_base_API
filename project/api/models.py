71652e9998248b4303e7006de6c03df570973fb2
brajesh708 <brajeshmewada694@gmail.com>
2024-08-07 16:40:39 +0530

    models

--- project/api/models.py
+++ project/api/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+
+class Snippet(models.Model):
+    created = models.DateTimeField(auto_now_add=True)
+    title = models.CharField(max_length=100, blank=True, default='')
+    code = models.TextField()
+    linenos = models.BooleanField(default=False)
+    language = models.CharField(max_length=100)
+    style = models.CharField( max_length=100)
+    
+    def __str__(self):
+        return self.title
+
+ 
