55f28e8fdb30ccf170549c52a986ee2de0b16380
brajesh708 <brajeshmewada694@gmail.com>
2024-12-17 22:23:39 +0530

    update

--- project/api/views.py
+++ project/api/views.py
@@ -10,7 +10,7 @@ class SnippetList(generics.ListCreateAPIView):
     queryset = Snippet.objects.all()
     serializer_class = SnippetSerializer
 
-
 class SnippetDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Snippet.objects.all()
     serializer_class = SnippetSerializer
+    
